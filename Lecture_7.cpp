249ea0a5ec14c48769f5dd9ba446bc1ded3da6b9
Parvati kumari <parvati000777@gmail.com>
2024-06-22 21:44:51 +0530

    array part1

---
+++ Lecture_7.cpp
@@ -0,0 +1,40 @@
+//Arrays....part 1
+
+//--------------------------------------------------------------------------------------------------------
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+//int x;
+//variable name x of type int --> it can store a single integer which means  x can either store 1 or 2 or any single integer
+
+
+//Arrays can be used to store mutliple values in a single variable name
+
+
+//=========================================================================================================
+
+//int a[5];
+//this means it is an array of name which can store multiple integers. It can store 6 integers
+
+//NOTE:- the indexing of array starts from 0 and not 1.
+
+//e;ements starting from0-5
+//The first element will have an index 0 ---> a[0]
+//the second element will have an index1 ---> a[1]
+
+
+int a,b,c,d;
+cout<<"Give 4 integers a, b, c, d :- "<<endl;
+cin>>a>>b>>c>>d;
+
+cout<<"The inetegers in reverse order are: "<<d<<c<<b<<a;
+
+return 0;
+
+
+
+}
